22e4499e1a373c89ccfd0b253dea76f4c35b0617
jennuwine <jennherrarte93@gmail.com>
2021-02-02 20:20:18 -0800

    Set up routes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const indexRouter = require('./Routes/index')
+const userRouter = require('./Routes/user')
 const methodOverride = require('method-override');
 
 const PORT = 3000; 
 
-
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res) {
-res.send('Hello')
-})
+app.use('/', indexRouter)
+app.use('/notpotions', userRouter)
 
 app.listen(PORT, () => console.log('I am working'));
+
