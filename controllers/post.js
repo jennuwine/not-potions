8a3565397dc16f6cd83aa8b00cb3b7461cee1aa0
jennuwine <jennherrarte93@gmail.com>
2021-02-02 23:54:34 -0800

    form post function is working

--- controllers/post.js
+++ controllers/post.js
@@ -6,13 +6,18 @@ const addPost = (req, res) => {
   res.render('newpost/newpostform')
 }
 
-// const newPost = async (req, res) => {
+ const newPost = async (req, res) => {
+  await db.post.create(req.body,(err, createdPost) => {
+  if(err) return res.redirect('/notpotions/addpotion')
+  createdPost.save()
+  console.log(createdPost)
+  res.redirect('/notpotions');
+ })
  
-// }
+}
 
 module.exports = {
    addPost,
-  //  newPost
+   newPost
  }
 
- 
